8902b45575ddcf0b1cc22c7804eee0647377262e
Ibad <ibrehman1995@hotmail.com>
2016-04-14 01:06:44 +0500

    breaking in to lexemes

---
+++ LexicalAnalyzer/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace LexicalAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(313, 241);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(525, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "LEXEMES";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(404, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "LEXEMES";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(313, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(537, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "TOKEN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(666, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "DES";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(385, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(328, 121);
+            this.listBox1.TabIndex = 10;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(385, 181);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(328, 121);
+            this.listBox2.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(760, 323);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+
+    }
+}
+
---
+++ LexicalAnalyzer/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LexicalAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string spacemodifier(string buffer)
+        {
+            //brackets modifier
+            buffer = buffer.Replace("(", " ( ");
+            buffer = buffer.Replace(")", " ) ");
+            buffer = buffer.Replace("{", " { ");
+            buffer = buffer.Replace("}", " } ");
+            buffer = buffer.Replace("[", " [ ");
+            buffer = buffer.Replace("]", " ] ");
+            
+
+            //terminator modifier
+            buffer = buffer.Replace(";", " ; ");
+            buffer = buffer.Replace(",", " , ");
+
+            //logical operator modifier
+            buffer = buffer.Replace("=", " = ");
+            buffer = buffer.Replace("+", " - ");
+            buffer = buffer.Replace("*", " * ");
+            buffer = buffer.Replace("/", " / ");
+            buffer = buffer.Replace("<", " < ");
+            buffer = buffer.Replace(">", " > ");
+            buffer = buffer.Replace("<=", " <= ");
+            buffer = buffer.Replace(">=", " >= ");
+            buffer = buffer.Replace("&", " & ");
+            buffer = buffer.Replace("|", " | ");
+            buffer = buffer.Replace("&&", " && ");
+            buffer = buffer.Replace("||", " || ");
+            
+            //data types modifier
+            buffer = buffer.Replace("main", " main ");
+            buffer = buffer.Replace("int", " int ");
+            buffer = buffer.Replace("long", " long ");
+            buffer = buffer.Replace("float", " float ");
+            buffer = buffer.Replace("double", " double ");
+            buffer = buffer.Replace("decimal", " decimal ");
+            buffer = buffer.Replace("string", " string ");
+            buffer = buffer.Replace("char", " char ");
+
+            //condition modifier
+            buffer = buffer.Replace("if", " if ");
+            buffer = buffer.Replace("else", " else ");
+            buffer = buffer.Replace("for", " for ");
+            buffer = buffer.Replace("while", " while ");
+            buffer = buffer.Replace("elseif", " elseif ");
+
+            //statement modiifier
+            buffer = buffer.Replace("return", " return ");
+            
+            //spaces remove with separator
+            buffer = buffer.Replace("  ", "");
+           
+            return buffer;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            string filename = @"C:\Users\Ansari\Desktop\testing.txt";
+            string stream = File.ReadAllText(filename);
+            stream = spacemodifier(stream);
+            textBox1.Text = stream;
+
+            string[] word = textBox1.Text.Split(' ');
+            listBox1.Items.Clear();
+
+            foreach (string item in word)
+            {
+                listBox1.Items.Add(item);
+            }
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+       
+    }
+}
